617522dcf64770d631e6b2dcd6a13fa5ff09b1ea
Leena <>
2025-01-10 18:01:23 +0530

    -- bug fixes in validator index file

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,7 @@
 const cryptoGraphy = require('./cryptography/encryption_decryption');
 const jsonWebToken = require('./json-web-token/jwt_token');
 const { authValidator, productValidator, catalogueValidator } = require('./validations');
-const { productValidator, catalogueValidator, sizeValidator, seriesValidator, categoryValidator } = require('./validations');
+const { sizeValidator, seriesValidator, categoryValidator } = require('./validations');
 
 
 module.exports = {
